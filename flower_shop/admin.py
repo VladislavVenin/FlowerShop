7b9fe43688d023549dd5c084b30d2a1cbc401c2a
mikrukovd <z123456689z@gmail.com>
2026-04-22 15:14:13 +0300

    Add admin

--- flower_shop/admin.py
+++ flower_shop/admin.py
@@ -1,3 +1,35 @@
+from .models import Flower, Bouquet, Order
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Bouquet)
+class BouquetAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'price', 'image_preview',
+        'is_available', 'created_at'
+    )
+    list_editable = ('price', )
+    editable_fields = ('name', 'price')
+    search_fields = ('name',)
+    filter_horizontal = ('flowers',)
+
+    def image_preview(self, obj):
+        html_template = (
+            '''<img src="{}" style="max-width: 200px;
+            max-height: 200px;" alt="{}" />'''
+        )
+        return format_html(html_template, obj.image.url, obj.name)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'bouquet', 'client_name', 'phone_number',
+        'address', 'created_at'
+    )
+    search_fields = ('client_name', 'phone_number', 'address')

82dca46f22bdb72f42928235937022e3ba4dc912
VladislavVenin <d4spda@yandex.ru>
2026-04-24 23:50:10 +1100

    revert index changes

--- static/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const quizButton = document.querySelector('.banner__btn')
-    if(quizButton){
-        quizButton.addEventListener('click', function () {
-            window.location.href = "/quiz";
-        })
-    }
-
-})
